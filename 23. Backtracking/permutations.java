4610045ebac51fcd6fe2c0cf043da11f5b4d2750
Varadkar45 <s.varadkar2001@gmail.com>
2024-04-18 22:46:29 +0530

    Update Backtracking

---
+++ 23. Backtracking/permutations.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class permutations {
+    public static void findPermutation(String str, String ans) {
+        //base case
+        if(str.length()==0) {
+            System.out.println(ans);
+        }
+        //Recursion
+        for(int i = 0; i<str.length(); i++) {
+
+            char curr = str.charAt(i);
+            //"abcde"=>"ab"+"de"="abde"
+            String NewStr = str.substring(0, i)+str.substring(i+1);
+            findPermutation(NewStr, ans+curr);
+        }
+    }
+    public static void main(String args[]) {
+        String str = "abc";
+        findPermutation(str, "");
+
+    }
+}
