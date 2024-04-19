57c48d5d2afea5f456d998c14a8f7a4dfe99fbba
Varadkar45 <s.varadkar2001@gmail.com>
2024-04-19 10:55:49 +0530

    Update Backtracking

--- 23. Backtracking/permutations.java
+++ 23. Backtracking/permutations.java
@@ -5,9 +5,8 @@ public class permutations {
         if(str.length()==0) {
             System.out.println(ans);
         }
-        //Recursion
+        //Recursion -T.C-> O(n*n!)
         for(int i = 0; i<str.length(); i++) {
-
             char curr = str.charAt(i);
             //"abcde"=>"ab"+"de"="abde"
             String NewStr = str.substring(0, i)+str.substring(i+1);
