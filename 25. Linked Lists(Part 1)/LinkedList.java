8f3c9da6f0e58a4af0c42329e5b2256c85a545b5
Varadkar45 <s.varadkar2001@gmail.com>
2024-04-25 07:58:25 +0530

    Update LinkedList

--- 25. Linked Lists(Part 1)/LinkedList.java
+++ 25. Linked Lists(Part 1)/LinkedList.java
@@ -75,6 +75,43 @@ public class LinkedList {
         System.out.println();
     }
 
+    public int removeFirst() {
+        if(size==0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if(size==1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+        if(size==0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if(size==1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        //prev:i=size-2
+        Node prev = head;
+        for(int i = 0; i<size-2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;//tail.data
+        prev.next = null;
+        size--;
+        return val;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         // ll.print();
@@ -94,6 +131,11 @@ public class LinkedList {
         ll.add(2,3);
         ll.print();
         System.out.println(size);
-
+        ll.removeFirst();
+        ll.print();
+        System.out.println(size);
+        ll.removeLast();
+        ll.print();
+        System.out.println(size);
     }
 }
