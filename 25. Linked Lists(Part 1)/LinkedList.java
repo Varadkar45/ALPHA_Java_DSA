12b1ad7da0300059b86fd4a208871df00addd657
Varadkar45 <s.varadkar2001@gmail.com>
2024-04-25 06:42:56 +0530

    Update LinkedList

--- 25. Linked Lists(Part 1)/LinkedList.java
+++ 25. Linked Lists(Part 1)/LinkedList.java
@@ -11,10 +11,12 @@ public class LinkedList {
 
     public static Node head;
     public static Node tail;
+    public static int size;// for size calculation
 
     public void addFirst(int data) {
         //step 1 create new node
         Node newNode = new Node(data);
+        size++;//for adding size
         if(head==null) {
             head = tail = newNode;
             return;
@@ -29,6 +31,7 @@ public class LinkedList {
     public void addLast(int data) {
         //step 1 create new node
         Node newNode = new Node(data);
+        size++;
         if(head==null) {
             head = tail = newNode;
             return;
@@ -39,6 +42,26 @@ public class LinkedList {
         //step 3 head = newNode
         tail = newNode;
     }
+    //Add in the middle
+    public void add(int idx, int data) {
+        if(idx==0) {
+            addFirst(data);
+            return;
+        }
+        size++;
+
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while(i<idx-1) {
+            temp = temp.next;
+            i++;
+        }
+        //i=idx-1; temp->prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
     public void print() {
         if(head==null) {
             System.out.println("Linked List is empty");
@@ -54,16 +77,23 @@ public class LinkedList {
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.print();
-        ll.addFirst(1);
-        ll.print();
-        ll.addFirst(2);
-        ll.print();
+        // ll.print();
+        // ll.addFirst(1);
+        // ll.print();
+        // ll.addFirst(2);
+        // ll.print();
 
-        ll.addLast(3);
-        ll.print();
+        // ll.addLast(3);
+        // ll.print();
+        // ll.addLast(4);
+        // ll.print();
+        ll.addFirst(2);
+        ll.addFirst(1);
         ll.addLast(4);
+        ll.addLast(5);
+        ll.add(2,3);
         ll.print();
+        System.out.println(size);
 
     }
 }
